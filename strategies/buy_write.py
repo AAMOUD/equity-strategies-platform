5408454a61f75103b4c6f11e8df242907a507e6f
AAMOUD <aicha.aamoud09@gmail.com>
2026-05-11 21:21:52 +0200

    normalize buy-write option P&L by S_prev instead of S0

--- strategies/buy_write.py
+++ strategies/buy_write.py
@@ -62,7 +62,7 @@ class BuyWriteStrategy(BaseStrategy):
                 if date == t1:
                     payoff_call = max(S_curr - strike, 0)
                     premium_fv = premium * np.exp(r * T)
-                    ret_call = (premium_fv - payoff_call) / S0
+                    ret_call = (premium_fv - payoff_call) / S_prev
                     total_ret = ret_stock + ret_call - tx_cost
                 else:
                     total_ret = ret_stock
